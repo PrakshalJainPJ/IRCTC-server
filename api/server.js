3b69aae362caba5ad6d4fb9321a95bdb44454edd
Prakshal Jain <prakshaljainofficial@gmail.com>
2024-12-29 19:32:20 +0530

    For deploy 2

--- api/server.js
+++ api/server.js
@@ -1,9 +1,9 @@
-// import express from 'express';
-// import cors from 'cors';
-// import fetch from 'node-fetch';
-const express = require('express')
-const cors = require('cors')
-const nodemon = require('nodemon')
+import express from 'express';
+import cors from 'cors';
+import fetch from 'node-fetch';
+// const express = require('express')
+// const cors = require('cors')
+// const nodemon = require('nodemon')
 const app = express();
 app.use(cors());
 app.get('/api/train-status/:trainNo', async (req, res) => {
