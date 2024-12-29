479e6c571221e1cc84cd80bf8dba81e4eac88940
Prakshal Jain <prakshaljainofficial@gmail.com>
2024-12-29 18:00:49 +0530

    Server ready

---
+++ server.js
@@ -0,0 +1,15 @@
+// import express from 'express';
+// import cors from 'cors';
+// import fetch from 'node-fetch';
+const express = require('express')
+const cors = require('cors')
+const nodemon = require('nodemon')
+const app = express();
+app.use(cors());
+app.get('/api/train-status/:trainNo', async (req, res) => {
+    let url = `https://www.railyatri.in/live-train-status/${req.params.trainNo}`;
+    let fetchState = await fetch(url);
+    const data = await fetchState.text();
+    res.send(data);
+});
+app.listen(3000);
