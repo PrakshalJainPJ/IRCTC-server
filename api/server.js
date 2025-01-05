c96337e3771ae74cd2a12e1fcfa00477c722e92b
Prakshal Jain <prakshaljainofficial@gmail.com>
2025-01-05 23:08:19 +0530

    API Changes for running status

--- api/server.js
+++ api/server.js
@@ -1,15 +1,19 @@
 import express from 'express';
 import cors from 'cors';
 import fetch from 'node-fetch';
-// const express = require('express')
-// const cors = require('cors')
-// const nodemon = require('nodemon')
+
 const app = express();
 app.use(cors());
 app.get('/api/train-status/:trainNo/:startDay', async (req, res) => {
-    let url = `https://www.railyatri.in/live-train-status/${req.params.trainNo}?start_day=${req.params.startDay}`;
-    let fetchState = await fetch(url);
-    const data = await fetchState.text();
-    res.send(data);
+    let TrainStatusUrl = `https://livestatus.railyatri.in/api/v3/train_eta_data/${req.params.trainNo}/-1.json?start_day=${req.params.startDay}`;
+    let fetchStatus = await fetch(TrainStatusUrl);
+    const dataTrainStatus = await fetchStatus.json();
+    res.send(dataTrainStatus);
+});
+app.get('/api/train-name-number', async (req, res) => {
+    let trainNameNumberUrl = 'https://enquiry.indianrail.gov.in/mntes/javascripts/train_data.js?v=2';
+    let fetchTrainNameNumber = await fetch(trainNameNumberUrl);
+    const TrainNameNumberData = await fetchTrainNameNumber.text();
+    res.send(TrainNameNumberData);
 });
 export default app;
