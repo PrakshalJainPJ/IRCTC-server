166809010adfadc6bab2f3c2c19ab910e0a0880b
Prakshal Jain <prakshaljainofficial@gmail.com>
2025-01-26 19:34:52 +0530

    PNR API URL Updated

--- api/server.js
+++ api/server.js
@@ -17,7 +17,7 @@ app.get('/api/train-name-number/:train', async (req, res) => {
     res.send(TrainNameNumberData);
 });
 app.get('/api/check-pnr-status/:pnr', async (req, res) => {
-    let pnrStatusCheckUrl = `https://www.railyatri.in/_next/data/6433c23273df72096a45d1631h6e91b2/m/pnr-status/${req.params.pnr}.json`;
+    let pnrStatusCheckUrl = `https://www.railyatri.in/_next/data/6433c23262ce72096a45d1631h4g92c2/m/pnr-status/${req.params.pnr}.json`;
     let fetchPnrStatus = await fetch(pnrStatusCheckUrl);
     const PnrStatusData = await fetchPnrStatus.json();
     res.send(PnrStatusData);
