5a1f9c487d4ce68c2c83a153466202e364ea3abd
Prakshal Jain <prakshaljainofficial@gmail.com>
2026-07-04 21:39:05 +0530

    Added allowed origins using CORS

--- api/server.js
+++ api/server.js
@@ -4,7 +4,21 @@ import fetch from 'node-fetch';
 import cookieParser from 'cookie-parser';
 
 const app = express();
-app.use(cors());
+
+const whitelist = ['https://myrailmate.vercel.app/search-train', 'https://myrailmate.vercel.app/train-schedule', 'https://myrailmate.vercel.app/train-status'];
+
+app.use(cors({
+  origin: function (origin, callback) {
+    // allow requests with no origin (like mobile apps or curl requests)
+    if (!origin) return callback(null, true);
+    
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  }
+}));
 app.use(cookieParser());
 
 import apiRoutes from './src/routes/apiRoutes.js';
